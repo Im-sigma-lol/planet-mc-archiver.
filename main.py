7938a17007a732f7ae6afba93fd5f45dd7b17cda
Reuploads <sigmadarling3@gmail.com>
2025-06-01 07:19:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def get_user_projects(username):
     while True:
         url = f"{BASE_URL}/member/{username}/?p={page}"
         print(f"[INFO] Checking: {url}")
-        r = requests.get(url)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36'}
+        r = requests.get(url, headers=headers)
         if r.status_code != 200:
             print(f"[ERROR] Unable to access page: {url}")
             break
